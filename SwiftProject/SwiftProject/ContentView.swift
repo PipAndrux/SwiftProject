c1ef39bfb44ebd30c0be39432c9f28cde0765650
Andrea Pepe <pepeeandrea22@gmail.com>
2025-09-30 17:42:11 +0200

    Prime prove comandi

--- SwiftProject/SwiftProject/ContentView.swift
+++ SwiftProject/SwiftProject/ContentView.swift
@@ -1,24 +1,18 @@
-//
-//  ContentView.swift
-//  SwiftProject
-//
-//  Created by d7 on 30/09/25.
-//
-
 import SwiftUI
 
 
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 10){
+            Text("Lissone, MB")
+            Text("Where do              we go today?")
+                .font(.system(size: 48, weight: .bold))
+            Image(systemName: "")
+            Spacer()
+            }.padding(.top, 20)
         }
-        .padding()
-    }
 }
 
 #Preview {
