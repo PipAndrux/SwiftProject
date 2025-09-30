1433bfb3fa1586d83924570b010ff29cfb598509
Andrea Pepe <pepeeandrea22@gmail.com>
2025-09-30 16:35:14 +0200

    Prima parte del progetto

---
+++ SwiftProject/SwiftProject/SwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftProjectApp.swift
+//  SwiftProject
+//
+//  Created by d7 on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
